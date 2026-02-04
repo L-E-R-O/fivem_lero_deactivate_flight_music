79f1600d01142590b3980fffc9c86d199773c871
root <root@v2202507285981359839.powersrv.de>
2026-02-04 19:29:17 +0100

    first commit

---
+++ client.lua
@@ -0,0 +1,6 @@
+Citizen.CreateThread(function()
+    while true do
+        SetAudioFlag('DisableFlightMusic', true)
+        Citizen.Wait(0)
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+
+fx_version 'bodacious'
+game 'gta5'
+
+author 'LERO'
+description 'Deactivates the flight music in FiveM'
+version '1.0.0'
+
+client_script 'client.lua'
